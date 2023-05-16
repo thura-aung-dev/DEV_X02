ac054574eb712be0c0d385e0139d535ad2717813
thura-aung-dev <impxxxaung@gmail.com>
2023-05-16 22:57:28 +0630

    align ui

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:dev_x02/widgets/r1_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,58 +17,88 @@ var data4="passed%20directly";
       appBar: AppBar(
         title: const Text('GetX02App'),
       ),
-      body: Column(
-        children: [
-          const TextField(
-            maxLines: 1,
-          ),
-          Row(
+      body: Center(
+        child:  IntrinsicHeight(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              TextButton(
-                  onPressed: () => Get.to(const R1Page()),
-                  style: const ButtonStyle(
-                    alignment: Alignment.center,
-                  ),
-                  child: const Text('R1')),
-              const VerticalDivider(
-                width: 20,
-                thickness: 1,
-                indent: 20,
-                endIndent: 0,
-                color: Colors.grey,
+              // Center(
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(8.0),
+              //     child: SizedBox(
+              //       width: 250,
+              //       height: 50,
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           enabledBorder: OutlineInputBorder(
+              //             borderSide:const BorderSide(
+              //               width: 1,
+              //               color: Colors.grey
+              //             ),
+              //             borderRadius: BorderRadius.circular(50.0),
+              //           )
+              //         ),
+              //         maxLines: 1,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              IntrinsicHeight(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextButton(
+                        onPressed: () => Get.to(const R1Page()),
+                        style: const ButtonStyle(
+                          alignment: Alignment.center,
+                        ),
+                        child: const Text('R1')),
+                    const VerticalDivider(
+                      width: 20,
+                      thickness: 1,
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.grey,
+                    ),
+                    TextButton(
+                        onPressed: () => Get.toNamed('/r2_page',
+                            parameters: {'paraVal': 'I am from parameter.'}),
+                        style: const ButtonStyle(
+                          alignment: Alignment.center,
+                        ),
+                        child: const Text('R2')),
+                  ],
+                ),
               ),
-              TextButton(
-                  onPressed: () => Get.toNamed('/r2_page',
-                      parameters: {'paraVal': 'I am from parameter.'}),
-                  style: const ButtonStyle(
-                    alignment: Alignment.center,
-                  ),
-                  child: const Text('R2')),
+              const SizedBox(
+                  width: 150,
+                  child:  Divider(height: 1, thickness: 1, endIndent: 0,color:Colors.grey,)),
+              IntrinsicHeight(
+                child: Row(
+                  children: [
+                    TextButton(
+                        onPressed: () => Get.toNamed('/r3_page',
+                            arguments: {'argVal': data3}),
+                        child: const Text('R3')),
+                    const VerticalDivider(
+                      width: 20,
+                      thickness: 1,
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.grey,
+                    ),
+                    TextButton(
+                        onPressed: () => Get.toNamed('/r4_page/$data4'),
+                        style: const ButtonStyle(
+                          alignment: Alignment.center,
+                        ),
+                        child: const Text('R4')),
+                  ],
+                ),
+              ),
             ],
           ),
-          const Divider(height: 1, thickness: 2, endIndent: 20),
-          Row(
-            children: [
-              TextButton(
-                  onPressed: () => Get.toNamed('/r3_page',
-                      arguments: {'argVal': data3}),
-                  child: const Text('R3')),
-              const VerticalDivider(
-                width: 20,
-                thickness: 2,
-                indent: 20,
-                endIndent: 0,
-                color: Colors.black,
-              ),
-              TextButton(
-                  onPressed: () => Get.toNamed('/r4_page/$data4'),
-                  style: const ButtonStyle(
-                    alignment: Alignment.center,
-                  ),
-                  child: const Text('R4')),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/widgets/r3_page.dart
+++ lib/widgets/r3_page.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class R3Page extends StatelessWidget{
